0db0c124ae157355bacddf1a8e7cb6614e3d6e4a
Shira Yogev <54641048+May2303@users.noreply.github.com>
2024-05-27 18:59:11 +0300

    Update mync.c

--- OS_exe2/mync.c
+++ OS_exe2/mync.c
@@ -2,97 +2,44 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
-#define BUFFER_SIZE 1024
-
-void print_error() {
-    fprintf(stderr, "Usage: mync -e \"program\"\n");
-    exit(EXIT_FAILURE);
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 3 || strcmp(argv[1], "-e") != 0) {
-        print_error();
+int main(int argc, char *argv[])
+{
+    if (argc != 3 || strcmp(argv[1], "-e") != 0)
+    {
+        printf("Usage: %s -e \"<program> <arguments>\"\n", argv[0]);
+        return 1;
     }
 
-    char *program = argv[2];
+    // Split the second argument into program name and arguments
+    char *program = strtok(argv[2], " ");
+    char *arguments = strtok(NULL, "");
 
-    int pipe_in[2];
-    int pipe_out[2];
-
-    if (pipe(pipe_in) == -1 || pipe(pipe_out) == -1) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
+    char *new_argv[] = {program, arguments, NULL};
 
     pid_t pid = fork();
 
-    if (pid == -1) {
+    if (pid < 0)
+    {
         perror("fork");
-        exit(EXIT_FAILURE);
+        return 1;
     }
+    else if (pid == 0)
+    {
+        // Child process
+        execvp(program, new_argv);
 
-    if (pid == 0) { // Child process
-        close(pipe_in[1]);
-        close(pipe_out[0]);
-
-        if (dup2(pipe_in[0], STDIN_FILENO) == -1 || dup2(pipe_out[1], STDOUT_FILENO) == -1) {
-            perror("dup2");
-            exit(EXIT_FAILURE);
-        }
-
-        close(pipe_in[0]);
-        close(pipe_out[1]);
-
-        execlp("/bin/sh", "sh", "-c", program, NULL);
-        perror("execlp");
-        exit(EXIT_FAILURE);
-    } else { // Parent process
-        close(pipe_in[0]);
-        close(pipe_out[1]);
-
-        char buffer[BUFFER_SIZE];
-        ssize_t bytes_read;
-
-        while ((bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0) {
-            if (write(pipe_in[1], buffer, bytes_read) == -1) {
-                perror("write");
-                exit(EXIT_FAILURE);
-            }
-
-            bytes_read = read(pipe_out[0], buffer, BUFFER_SIZE);
-
-            if (bytes_read == -1) {
-                perror("read");
-                exit(EXIT_FAILURE);
-            }
-
-            if (write(STDOUT_FILENO, buffer, bytes_read) == -1) {
-                perror("write");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        if (bytes_read == -1) {
-            perror("read");
-            exit(EXIT_FAILURE);
-        }
-
-        close(pipe_in[1]);
-        close(pipe_out[0]);
-
+        // If execvp returns, there was an error
+        perror("execvp");
+        return 1;
+    }
+    else
+    {
+        // Parent process
         int status;
-        if (waitpid(pid, &status, 0) == -1) {
-            perror("waitpid");
-            exit(EXIT_FAILURE);
-        }
-
-        if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-            fprintf(stderr, "Child process exited abnormally\n");
-            exit(EXIT_FAILURE);
-        }
+        waitpid(pid, &status, 0);
     }
 
     return 0;
