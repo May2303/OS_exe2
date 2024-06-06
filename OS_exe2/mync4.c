2e244c2c3b97583c2edbb72d6bd20cc8e63572ce
Shira Yogev <54641048+May2303@users.noreply.github.com>
2024-06-06 23:01:09 +0300

    Add files via upload

---
+++ OS_exe2/mync4.c
@@ -0,0 +1,260 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <signal.h>
+#include <errno.h>
+#include <signal.h>
+
+int kill(pid_t pid, int sig); //CHECK FOR ME 
+
+int sender_fd= -1;
+int receiver_fd = -1; // Global variable to receiver socket file descriptor
+int timeout = 0; // Global variable to timeout
+
+void close_receiver(int sig) {
+    (void)sig; //unused parameter warning
+    if (receiver_fd != -1) { // Check if the receiver socket fd is valid (not -1)
+        close(receiver_fd); // Close the receiver socket fd
+        receiver_fd = -1; // Reset the receiver socket fd to indicate closure
+    }
+}
+
+// Signal handler for timeout
+void handle_timeout(int sig) {
+    (void)sig;
+    printf("Timeout reached. Closing connection.\n");
+    close_receiver(SIGINT); // Close the connection
+    kill(0, SIGKILL); // Kill all processes in the current process group
+}
+
+int create_udp_server(int port) {
+    int sockfd;
+    struct sockaddr_in myaddr;
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+    printf("UDP server socket created on port %d\n", port);
+
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_port = htons(port);
+    myaddr.sin_addr.s_addr = INADDR_ANY;
+    memset(&(myaddr.sin_zero), '\0', 8);
+
+    if (bind(sockfd, (struct sockaddr *)&myaddr, sizeof(struct sockaddr)) == -1) {
+        perror("bind");
+        exit(1);
+    }
+     printf("UDP server bind\n");
+
+    receiver_fd = sockfd; // Assigning the receiver socket file descriptor
+    printf("the fd is: %d\n",receiver_fd);
+    return sockfd;
+}
+
+int create_udp_client(char *hostname, int port) {
+    struct sockaddr_in theiraddr;
+    struct hostent *he;
+    int sockfd;
+
+    if ((he = gethostbyname(hostname)) == NULL) {
+        perror("gethostbyname");
+        exit(1);
+    }
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+    printf("UDP client socket created on port %d\n", port);
+
+    theiraddr.sin_family = AF_INET;
+    theiraddr.sin_port = htons(port);
+    theiraddr.sin_addr = *((struct in_addr *)he->h_addr_list[0]);
+    memset(&(theiraddr.sin_zero), '\0', 8);
+
+    printf("Attempting to connect to server at %s:%d\n", inet_ntoa(*(struct in_addr *)he->h_addr_list[0]), port);
+
+    if (connect(sockfd, (struct sockaddr *)&theiraddr, sizeof(struct sockaddr)) == -1) {
+        perror("connect");
+        exit(1);
+    }
+    printf("UDP client connected to server\n");
+    return sockfd;
+}
+/*
+redirect the standard input (stdin) and standard output (stdout)
+of the current process to different file descriptors,
+specified by new_stdin and new_stdout.
+*/
+void redirect_io(int new_stdin, int new_stdout) {
+    if (new_stdin != -1) {
+        //duplicates the file descriptor new_stdin onto the file descriptor for stdin using dup2
+        dup2(new_stdin, STDIN_FILENO);
+        close(new_stdin);
+    }
+    if (new_stdout != -1) {
+        //duplicates the file descriptor new_stdout onto the file descriptor for stdout using dup2
+        dup2(new_stdout, STDOUT_FILENO);
+        close(new_stdout);
+    }
+}
+
+void parse_argument(const char *arg, char *host, int *port) {
+    // Skip the "UDPC" prefix
+    arg += 4;
+
+    // Find the position of the comma
+    const char *comma = strchr(arg, ',');
+    
+    // If no comma is found, return an error or handle it accordingly
+    if (comma == NULL) {
+        fprintf(stderr, "Invalid UDP argument format: %s\n", arg);
+        exit(EXIT_FAILURE);
+    }
+    
+    // Copy the host part of the argument
+    strncpy(host, arg, comma - arg);
+    host[comma - arg] = '\0'; // Null-terminate the string
+    
+    // Parse the port part of the argument
+    *port = atoi(comma + 1);
+    printf("Debug: Host = %s, Port = %d\n", host, *port); // Debug print
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3 || strcmp(argv[1], "-e") != 0) {
+         printf("Usage: %s -e \"<program> <arguments>\" [-i TCPS<port>|UDPS<port>] [-o TCPC<ip,port>|UDPC<ip,port>] [-b TCPS<port>] [-t timeout]\n", argv[0]);
+        return 1;
+    }
+
+    printf("Number of arguments: %d\n", argc);
+    printf("Arguments:\n");
+
+
+    char *exec_command = argv[2];
+    int new_stdin = -1;
+    int new_stdout = -1;
+
+    signal(SIGCHLD, close_receiver);
+    
+    // Go over the args we get to find the relevant flags. 
+        
+        // -i flag
+       if (strcmp(argv[3], "-i") == 0){
+            printf("argv[3]-flag: %s\n", argv[3]);
+            printf("argv[4]-port server: %s\n", (argv[4]));
+
+          //  if(strncmp(argv[4], "UDPS",4) == 0) {
+                //int port = atoi(argv[4] + 4);
+                int port = 4050;
+                printf("the port of the server: %d\n", port);
+                new_stdin = create_udp_server(port);
+                sender_fd=new_stdin;
+          //  }
+
+            // -t flag
+           if (strcmp(argv[5], "-t") == 0) {
+                timeout = atoi(argv[6])*6;
+                printf("Timeout is %d seconds\n", timeout);
+
+                if (timeout > 0) {
+                    alarm(timeout);
+                    signal(SIGALRM, handle_timeout); // Change handle_alarm to handle_timeout
+                }
+           }
+        }   
+        
+        // -o flag    
+        else if (strcmp(argv[3], "-o") == 0 ){
+            char *arg = argv[4];
+            const char *prefix = "UDPC";
+            printf("argv[3]-flag type: %s\n", argv[3]);
+
+         //   if(strncmp(arg, prefix, 4) == 0){ 
+                char host[256];
+                int port;
+                parse_argument(argv[4] , host, &port);
+                new_stdout = create_udp_client(host, port); 
+                receiver_fd = new_stdout;
+         //   }
+    }
+
+
+   // Split the second argument into program name and arguments
+    char *program = strtok(argv[2], " ");
+    char *arguments = strtok(NULL, "");
+    int move;
+    char *new_argv[] = {program, arguments, NULL};
+ 
+// Fork to execute the game program in the child process
+    pid_t pid = fork();
+    if (pid < 0) {
+        // Fork error
+        perror("fork");
+        return 1;
+    }
+    else if (pid == 0) {
+        // Child process: Execute the game program
+
+        if (new_stdin != -1) {
+        //duplicates the file descriptor new_stdin onto the file descriptor for stdin using dup2
+        dup2(new_stdin, STDIN_FILENO);
+        close(new_stdin);
+        }
+        if (new_stdout != -1) {
+        //duplicates the file descriptor new_stdout onto the file descriptor for stdout using dup2
+        dup2(new_stdout, STDOUT_FILENO);
+        close(new_stdout);
+        
+        }
+        
+        execvp(program, new_argv);
+        perror("execvp");
+        return 1;
+    
+    } else {
+        // Parent process: Wait for client input
+        while (1) {
+            // Once the game program prints "I won" or "I lost", exit the loop
+            // Example:
+            char buffer[1024];
+            ssize_t bytes_received = recvfrom(receiver_fd, buffer, sizeof(buffer), 0, NULL, NULL);
+            if (bytes_received > 0) {
+                buffer[bytes_received] = '\0'; // Null-terminate the received data
+                printf("Received: %s\n", buffer);
+                // Check for game over condition and break the loop if needed
+                if (strcmp(buffer, "I win\n") == 0){
+                    printf("I win\n");
+                    break;
+                } else if(strcmp(buffer, "I lost\n") == 0 ){
+                    printf("I lost\n");
+                    break;
+                }else if( strcmp(buffer, "Error\n") == 0 ) {
+                    printf("You sent invaild move\n");
+                    break;
+                }
+
+            } else if (bytes_received == 0) {
+                // Client closed connection
+                printf("Client closed connection\n");
+                break;
+            } else {
+                // Error in receiving data
+                perror("recvfrom");
+                break;
+            }
+        }
+        // Clean up resources and exit
+        if (receiver_fd != -1) {
+            close_receiver(receiver_fd);
+        }
+    }
+        return 0;
+    }
--- OS_exe2/ttt.c
+++ OS_exe2/ttt.c
@@ -1,127 +1,133 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-int print_error() {
-    printf("Error\n");
-    return -1;
-}
-
-void check_valid_input(int *strategy) {
-    int count[10] = {0}; // Array to count occurrences of each digit
-    for (int i = 0; i < 9; i++) {
-        if (strategy[i] < 1 || strategy[i] > 9 || count[strategy[i]] > 0) {
-            print_error();
-        }
-        count[strategy[i]]++;
-    }
-}
-
-int choose_move(int *board, int *strategy) {
-    int msd = strategy[0];
-    int lsd = strategy[8];
-    int used[9] = {0};  // Array to track used positions
-
-    // Mark positions already used on the board
-    for (int i = 0; i < 9; i++) {
-        if (board[i] == 11 || board[i] == 22) {
-            used[i] = 1;
-        }
+// Function to validate the strategy input
+int validate_strategy(const char *strategy) {
+    if (strlen(strategy) != 9) { 
+        return 0;
     }
 
-    // Find the first available move in the strategy
+    int digits[10] = {0};
     for (int i = 0; i < 9; i++) {
-        if (strategy[i] >= msd && strategy[i] <= lsd && !used[strategy[i] - 1]) {
-            used[strategy[i] - 1] = 1;
-            return strategy[i] - 1;
+        if (!isdigit(strategy[i]) || strategy[i] == '0') { 
+            return 0;
         }
+        int digit = strategy[i] - '0';
+        if (digits[digit] > 0) {
+            return 0;
+        }
+        digits[digit]++;
     }
 
-    // If all moves are used or strategy is invalid, return -1
-    return -1;
+    return 1;
 }
 
-int check_win(int *board, int player) {
-    int win_options[8][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+void print_board(char *board) {
+    printf("Board:\n");
+    printf(" %c | %c | %c \n", board[0], board[1], board[2]);
+    printf("---+---+---\n");
+    printf(" %c | %c | %c \n", board[3], board[4], board[5]);
+    printf("---+---+---\n");
+    printf(" %c | %c | %c \n", board[6], board[7], board[8]);
+}
+int check_winner(char *board, char mark) {
+    int win_conditions[8][3] = {
+        {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // rows
+        {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // columns
+        {0, 4, 8}, {2, 4, 6}             // diagonals
+    };
+
     for (int i = 0; i < 8; i++) {
-        int count = 0;
-        for (int j = 0; j < 3; j++) {
-            if (board[win_options[i][j]] == player) {
-                count++;
-            }
-        }
-        if (count == 3) {
+        if (board[win_conditions[i][0]] == mark &&
+            board[win_conditions[i][1]] == mark &&
+            board[win_conditions[i][2]] == mark) {
             return 1;
         }
     }
+
     return 0;
 }
 
-int check_draw(int *board) {
+int is_full(char *board) {
     for (int i = 0; i < 9; i++) {
-        if (board[i] >= 1 && board[i] <= 9) {
+        if (board[i] == ' ') {
             return 0;
         }
     }
     return 1;
 }
 
+void ttt(const char *strategy) {
+    if (!validate_strategy(strategy)) {
+        printf("Error\n");
+        return;
+    }
+
+    int move_order[9];
+    for (int i = 0; i < 9; i++) {
+        move_order[i] = strategy[i] - '0';
+    }
+
+    char board[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    char ai_mark = 'X';
+    char human_mark = 'O';
+
+    //print_board(board);  // Initial empty board
+
+    while (1) {
+        // AI move
+        for (int i = 0; i < 9; i++) {
+            int move = move_order[i] - 1;
+            if (board[move] == ' ') {
+                board[move] = ai_mark;
+                printf("%d\n", move_order[i]);
+                fflush(stdout);
+                break;
+            }
+        }
+
+       // print_board(board);  // Print board after AI move
+
+        if (check_winner(board, ai_mark)) {
+            printf("I win\n");
+            return;
+        }
+
+        if (is_full(board)) {
+            printf("DRAW\n");
+            return;
+        }
+
+        // Human move
+        int human_move;
+                if (scanf("%d", &human_move) != 1 || human_move < 1 || human_move > 9 || board[human_move - 1] != ' ') {
+            printf("Error\n");
+            return;
+        }
+        board[human_move - 1] = human_mark;
+
+      //  print_board(board);  // Print board after human move
+
+        if (check_winner(board, human_mark)) {
+            printf("I lost\n");
+            return;
+        }
+
+        if (is_full(board)) {
+            printf("DRAW\n");
+            return;
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        print_error();
+        printf("Error\n");
+        return 1;
     }
 
-    char *strategy_str = argv[1];
-    int strategy[9];
-
-    // Convert strategy string to integer array
-    for (int i = 0; i < 9; i++) {
-        strategy[i] = strategy_str[i] - '0';
-    }
-
-    check_valid_input(strategy);
-
-    int board[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-    for (int turn = 0; turn < 9; turn++) {
-        int AImove = choose_move(board, strategy);
-        printf("%d\n", AImove + 1); // Adjust for 1-based indexing
-        //fflush(stdout);
-        board[AImove] = 22; // 'X' represented as 22
-
-        if (check_win(board, 22)) { // 'X' represented as 22
-            printf("I win\n");
-            exit(EXIT_SUCCESS);
-        } else if (check_draw(board)) {
-            printf("DRAW\n");
-            exit(EXIT_SUCCESS);
-        }
-
-        int player_move;
-        scanf("%d", &player_move);
-        player_move--;
-
-        if (player_move < 0 || player_move >= 9 || board[player_move] < 1 || board[player_move] > 9) {
-            print_error();
-        }
-
-        board[player_move] = 11; // 'O' represented as 11
-
-        if (check_win(board, 11)) { // 'O' represented as 11
-            printf("I lost\n");
-            exit(EXIT_SUCCESS);
-
-        } else if (check_draw(board)) {
-            printf("DRAW\n");
-            exit(EXIT_SUCCESS);
-        }
-/*
-        AImove = choose_move(board, strategy);
-        printf("%d\n", AImove + 1); // Adjust for 1-based indexing
-       // fflush(stdout);
-
-*/
-        
-    }
+    ttt(argv[1]);
     return 0;
 }
